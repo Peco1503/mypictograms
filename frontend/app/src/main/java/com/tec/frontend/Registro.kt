f64a341a76928aee6cb800d146dcd6b698386230
Galindo4007 <105263859+Galindo4007@users.noreply.github.com>
2023-10-31 08:55:59 -0600

    Registro
    Ya esta la Pestaña de registro

--- frontend/app/src/main/java/com/tec/frontend/Registro.kt
+++ frontend/app/src/main/java/com/tec/frontend/Registro.kt
@@ -1,6 +1,5 @@
 package com.tec.frontend
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +8,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,6 +20,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,14 +32,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tec.frontend.ui.theme.FrontendTheme
 
-class InicioSesion : ComponentActivity() {
+class Registro : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,7 +49,7 @@ class InicioSesion : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Inicio()
+                    Registros()
                 }
             }
         }
@@ -54,10 +57,9 @@ class InicioSesion : ComponentActivity() {
 }
 
 @Composable
-fun Inicio() {
+fun Registros() {
     var text1 by remember { mutableStateOf("") }
     var text2 by remember { mutableStateOf("") }
-    val context = LocalContext.current
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF4169CF),
@@ -69,15 +71,17 @@ fun Inicio() {
         ) {
             Box(
                 modifier = Modifier
-                    .size(700.dp)
+                    .size(600.dp)
                     .background(Color.White),
                 contentAlignment = Alignment.TopCenter
             ) {
-                Column(verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally)
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                    )
                 {
                     Text(
-                        text = "Inicio Sesión",
+                        text = "Registro",
                         style = TextStyle(
                             fontSize = 65.sp,
                             fontWeight = FontWeight.Bold
@@ -88,7 +92,7 @@ fun Inicio() {
                     BasicTextField(
                         modifier = Modifier
                             .width(550.dp)
-                            .padding(top = 85.dp)
+                            .padding(top = 45.dp)
                             .border(2.dp, Color.Gray, MaterialTheme.shapes.medium),
                         value = text1,
                         onValueChange = {
@@ -141,23 +145,22 @@ fun Inicio() {
                             innerTextField()
                         }
                     )
+                    Spacer(modifier = Modifier.padding(top = 25.dp))
+                    TwoOptionsCheckBox()
                     Button(
                         modifier = Modifier
-                            .padding(top = 45.dp),
+                            .padding(top = 15.dp),
                         shape = RoundedCornerShape(30.dp),
                         colors = ButtonDefaults.buttonColors(Color(0xFFEE6B11)),
-                        onClick =
-                            {
-                                context.startActivity(Intent(context, Registro::class.java))
-                            },
-                        )
-                        {
-                            Text(
-                                text = "Iniciar Sesión",
-                                style = TextStyle(
-                                        fontSize = 35.sp
-                                )
+                        onClick = { /*TODO*/
+                        },
+                    )
+                    {
+                        Text(text = "Registrar",
+                            style = TextStyle(
+                                fontSize = 35.sp
                             )
+                        )
                     }
                 }
             }
@@ -165,5 +168,42 @@ fun Inicio() {
     }
 }
 
+@Composable
+fun TwoOptionsCheckBox() {
+    var option1CheckedState by remember { mutableStateOf(false) }
+    var option2CheckedState by remember { mutableStateOf(false) }
 
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Titulo     ", style = TextStyle(fontSize = 35.sp), modifier = Modifier.padding(25.dp))
+            Text(text = "Admin", style = TextStyle(fontSize = 35.sp),  modifier = Modifier.padding(25.dp))
+            Checkbox(
+                checked = option1CheckedState,
+                onCheckedChange = { option1CheckedState = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(25.dp)
+            )
+            Text(text = "Padre", style = TextStyle(fontSize = 35.sp), modifier = Modifier.padding(25.dp))
+            Checkbox(
+                checked = option2CheckedState,
+                onCheckedChange = { option2CheckedState = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(25.dp)
+            )
+        }
 
+        // Acciones basadas en la selección de opciones (puedes agregar lógica aquí)
+        if (option1CheckedState) {
+            // Opción 1 seleccionada
+        }
+
+        if (option2CheckedState) {
+            // Opción 2 seleccionada
+        }
+    }
+}
