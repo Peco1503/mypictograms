ace7124d4c7eddac08d54f1e96158145af6db3b7
Peco1503 <102172862+Peco1503@users.noreply.github.com>
2023-10-30 20:08:39 -0600

    Pantalla Inicio Sesion
    Pantalla Inicio Sesion: Alumnos

--- frontend/app/src/main/java/com/tec/frontend/AlumnosPaginaInicio.kt
+++ frontend/app/src/main/java/com/tec/frontend/AlumnosPaginaInicio.kt
@@ -2,11 +2,14 @@ package com.tec.frontend
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.text.TextStyle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,12 +21,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,43 +55,64 @@ class AlumnosPaginaInicio : ComponentActivity() {
 
 @Composable
 fun PantallaInicioAlum() {
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF4169CF)
     ) {
-        Column(
+        Box(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            contentAlignment = Alignment.Center
         ) {
-            Text("¿LIST@ PARA JUGAR?", fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color.White)
-            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "¿List@ para jugar?",
+                color = Color.White,
+                style = TextStyle(fontSize = 30.sp),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 180.dp)
+            )
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(10.dp))
+                    .width(821.dp)
+                    .height(341.dp)
                     .background(Color.White)
-                    .padding(16.dp)
-                    .width(250.dp)
+                    .border(1.dp, Color.Black),
+                contentAlignment = Alignment.Center
             ) {
                 Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text("Selecciona tu nombre", fontWeight = FontWeight.Bold)
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Button(onClick = {}) {
-                        Text("Alumno 1")
+                    Text(
+                        text = "Selecciona tu nombre",
+                        color = Color.Black,
+                        style = TextStyle(fontSize = 45.sp),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    listOf("Alumno 1").forEach { student ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .border(2.dp, Orange, RoundedCornerShape(10.dp))
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = student,
+                                style = TextStyle(fontSize = 35.sp, fontWeight = FontWeight.Normal)
+                            )
+
+
+                        }
                     }
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewPantallaInicioAlum() {
-    FrontendTheme {
-        PantallaInicioAlum()
     }
 }
