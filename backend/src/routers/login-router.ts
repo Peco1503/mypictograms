a0ce3afbe3e289404013508d3cf768af7939e2f8
pedroalonsoms <pedro123ben10@gmail.com>
2023-11-09 15:55:46 -0600

    add password hashing, make emails be unique, and add more password protection

--- backend/src/routers/login-router.ts
+++ backend/src/routers/login-router.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { Singleton } from "../db/connection";
 import { admins, parents } from "../db/schema";
 import z from "zod";
+import { hashPassword } from "../shared/utils";
 
 const loginRouter = express.Router();
 
@@ -14,24 +15,34 @@ loginRouter.post("/login", async (req, res) => {
     })
     .parse(req.body);
 
+  const hashedPassword = hashPassword(password);
+
   const db = await Singleton.getDB();
   const adminsFound = await db
     .select()
     .from(admins)
-    .where(and(eq(admins.user, user), eq(admins.password, password)));
+    .where(and(eq(admins.user, user), eq(admins.password, hashedPassword)));
 
   if (adminsFound.length > 0) {
-    res.json({ ...adminsFound[0], type: "ADMIN" });
+    res.json({
+      id: adminsFound[0].id,
+      user: adminsFound[0].user,
+      type: "ADMIN",
+    });
     return;
   }
 
   const parentsFound = await db
     .select()
     .from(parents)
-    .where(and(eq(parents.user, user), eq(parents.password, password)));
+    .where(and(eq(parents.user, user), eq(parents.password, hashedPassword)));
 
   if (parentsFound.length > 0) {
-    res.json({ ...parentsFound[0], type: "PARENT" });
+    res.json({
+      id: parentsFound[0].id,
+      user: parentsFound[0].user,
+      type: "PARENT",
+    });
     return;
   }
 
---
+++ backend/src/shared/utils.ts
@@ -0,0 +1,33 @@
+import { createHash } from "crypto";
+
+export const hashPassword = (password: string) => {
+  return createHash("sha256").update(password).digest("hex");
+};
+
+export const validatePassword = (password: string) => {
+  // https://stackoverflow.com/questions/12090077/javascript-regular-expression-password-validation-having-special-characters
+
+  if (password.length < 12) {
+    throw new Error("Password must be at least 12 characters");
+  }
+
+  const containsUppercase = /^(?=.*[A-Z]).*$/;
+  if (!containsUppercase.test(password)) {
+    throw new Error("Password must have at least one Uppercase Character.");
+  }
+
+  const containsLowercase = /^(?=.*[a-z]).*$/;
+  if (!containsLowercase.test(password)) {
+    throw new Error("Password must have at least one Lowercase Character.");
+  }
+
+  const containsNumber = /^(?=.*[0-9]).*$/;
+  if (!containsNumber.test(password)) {
+    throw new Error("Password must contain at least one Digit.");
+  }
+
+  const containsSymbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+  if (!containsSymbol.test(password)) {
+    throw new Error("Password must contain at least one Special Symbol.");
+  }
+};
