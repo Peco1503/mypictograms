d2e64af3d57a5b712254a02a33fd49d790607cba
pedroalonsoms <pedro123ben10@gmail.com>
2023-11-13 16:24:37 -0600

    added morgan

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,13 @@ import loginRouter from "./routers/login-router";
 import studentsRouter from "./routers/students-router";
 import adminsRouter from "./routers/admins-router";
 import parentsRouter from "./routers/parents-router";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use("/api", loginRouter);
 app.use("/api", studentsRouter);
 app.use("/api", adminsRouter);
