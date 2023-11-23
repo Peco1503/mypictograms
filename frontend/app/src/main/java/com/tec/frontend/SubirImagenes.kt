8aa62f4fc6bdeb36ee4c043dbb08bd30aaab354a
Felipegonac0 <felipegonac0@gmail.com>
2023-11-23 09:50:10 -0600

    Upload image page finished without API

--- frontend/app/src/main/java/com/tec/frontend/SubirImagenes.kt
+++ frontend/app/src/main/java/com/tec/frontend/SubirImagenes.kt
@@ -56,6 +56,7 @@ import com.tec.frontend.ui.theme.FrontendTheme
 import coil.compose.AsyncImage
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import com.tec.frontend.util.ImageUploader
@@ -71,11 +72,42 @@ class SubirImagenes : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     SubirImagenesPantalla()
+                    BackButtonUI()
                 }
             }
         }
     }
 }
+@Composable
+fun BackButtonUI() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
+        verticalAlignment = Alignment.Top) {
+        val context = LocalContext.current
+        Button( // Regresar a pantalla SeleccionNivel
+            shape = RectangleShape,
+            onClick = {
+                context.startActivity(
+                    Intent(
+                        context,
+                        SeleccionNivel::class.java
+                    )
+                )
+            },
+            modifier = Modifier
+                .width(116.dp)
+                .height(34.dp),
+            colors = ButtonDefaults.buttonColors(Orange)
+        ){
+            Text(
+                "ATRAS",
+                style = TextStyle(fontSize = 12.sp)
+            )
+        }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -163,6 +195,9 @@ fun SubirImagenesPantalla() {
 
                     // Categoria por dropdown list
                     ExposedDropdownMenuBox(
+                        modifier = Modifier
+                            .width(750.dp)
+                            .border(2.dp, Color.Gray, MaterialTheme.shapes.medium),
                         expanded = expanded.value,
                         onExpandedChange = {
                             expanded.value = !expanded.value
@@ -176,14 +211,24 @@ fun SubirImagenesPantalla() {
                                 ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded.value)
                             },
                             modifier = Modifier.menuAnchor()
+                                .width(750.dp)
+                                .background(Color.White),
+                            textStyle = TextStyle.Default.copy(fontSize = 28.sp, color = Color.Gray)
                         )
                         ExposedDropdownMenu(
+                            modifier = Modifier
+                                .width(750.dp)
+                                .border(2.dp, Color.Gray, MaterialTheme.shapes.medium)
+                                .background(Color.White),
                             expanded = expanded.value,
-                            onDismissRequest = { expanded.value = false}
+                            onDismissRequest = { expanded.value = false},
+
                         ) {
                             categoryNames.forEach {
                                 DropdownMenuItem(
-                                    text = { Text(text = it) },
+                                    modifier = Modifier
+                                        .width(850.dp),
+                                    text = { Text(text = it, fontSize = 28.sp) },
                                     onClick = {
                                         category.value = it
                                         expanded.value = false
